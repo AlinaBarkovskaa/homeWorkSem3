d4c9a714cc4bd0f0c556e265caf3788abecad871
AlinaBarkovskaa <a.barkovskaa@icloud.com>
2022-11-28 14:30:49 +0400

    improved task 19

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите пятизначное число: ");
-string n = Convert.ToString(Console.ReadLine());
+string n = Console.ReadLine();
 if(n.Length == 5)
 {
    if(n[0] == n[4] && n[1] == n[3])
