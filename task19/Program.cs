becd78632d57db178a142f546e7cdee44a69520d
AlinaBarkovskaa <a.barkovskaa@icloud.com>
2022-11-28 14:27:15 +0400

    Added task 19

---
+++ task19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+string n = Convert.ToString(Console.ReadLine());
+if(n.Length == 5)
+{
+   if(n[0] == n[4] && n[1] == n[3])
+   {
+       Console.WriteLine($"Число: {n} - палиндром.");
+   }else
+   {
+      Console.WriteLine($"Число: {n} - не палиндром.");
+   }
+}else
+{
+   Console.WriteLine("Введите пятизначное число");
+}
