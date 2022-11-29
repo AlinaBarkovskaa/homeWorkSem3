b9d51fd288cce0949cbc46c07d622f5d38a65054
AlinaBarkovskaa <a.barkovskaa@icloud.com>
2022-11-29 11:37:30 +0400

    added task 23

---
+++ task23/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void cubeTable(int[] n)
+{
+int length = n.Length;
+for(int i = 1; i < n.Length; i ++)
+  {
+    n[i] = Convert.ToInt32(Math.Pow(i, 3));
+  }
+  int rezult = n.Length;
+  for(int i = 1; i < rezult; i++)
+  {
+    Console.Write(n[i]+ " ");
+  }
+}
+int[] array = new int[n+1];
+cubeTable(array);
+
+
+
