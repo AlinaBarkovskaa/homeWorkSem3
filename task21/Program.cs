61ac246dca6fa7b93bc81e80c57687b2adbbb509
AlinaBarkovskaa <a.barkovskaa@icloud.com>
2022-11-29 11:47:56 +0400

    Apoved task

--- task21/Program.cs
+++ task21/Program.cs
@@ -11,4 +11,4 @@ Console.Write("Введите координату Y2: ");
 double y2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите координату Z2:");
 double z2 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine($"Расстояние между точками: {Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2))}");
+Console.WriteLine($"Расстояние между точками = {Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2))}");
--- task23/Program.cs
+++ task23/Program.cs
@@ -4,15 +4,10 @@ int n = Convert.ToInt32(Console.ReadLine());
 
 void cubeTable(int[] n)
 {
-int length = n.Length;
-for(int i = 1; i < n.Length; i ++)
+  for(int i = 1; i < n.Length; i ++)
   {
     n[i] = Convert.ToInt32(Math.Pow(i, 3));
-  }
-  int rezult = n.Length;
-  for(int i = 1; i < rezult; i++)
-  {
-    Console.Write(n[i]+ " ");
+     Console.Write(n[i]+ " ");
   }
 }
 int[] array = new int[n+1];
